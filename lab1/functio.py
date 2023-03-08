416229e3bf11a7e43999b8a6b7a807c40f82a262
NikitaKot1 <nikita_kotlyarov2002@mail.ru>
2023-03-08 11:27:28 +0300

    3d task, find y-max

--- lab1/functio.py
+++ lab1/functio.py
@@ -1,9 +1,11 @@
 
 def f_1(x, u):
     return (x + u**2)
+    #return 1 - u**2
 
 def f_2(x, u):
     return (u**3 + 2*x*u)
+    #return (1 + u**3) / (2*u)
 
 def f_3(x, u):
     return (x**2 + u**2)
--- lab1/main.py
+++ lab1/main.py
@@ -25,10 +25,13 @@ def task_1() -> PrettyTable:
         row = []
         row.append("{:^9.3f}".format(x_min))
         row.append("{:^9.5f}".format(analit.analit_1(x_min)))
-        if eul[i] > 10000000:
-            row.append("Too big")
+        if type(eul[i]) == int:
+            if eul[i] > 10000000:
+                row.append("Too big")
+            else:
+                row.append("{:^9.5f}".format(eul[i]))
         else:
-            row.append("{:^9.5f}".format(eul[i]))
+            row.append(eul[i])
 
         row.append("{:^9.5f}".format(picar1[i]))
         row.append("{:^9.5f}".format(picar2[i]))
@@ -62,43 +65,6 @@ def task_2() -> PrettyTable:
         row = []
         row.append("{:^9.3f}".format(x_min))
         row.append("{:^9.5f}".format(analit.analit_2(x_min)))
-        if eul[i] > 10000000:
-            row.append("Too big")
-        else:
-            row.append("{:^9.5f}".format(eul[i]))
-
-        row.append("{:^9.5f}".format(picar1[i]))
-        row.append("{:^9.5f}".format(picar2[i]))
-        row.append("{:^9.5f}".format(picar3[i]))
-        row.append("{:^9.5f}".format(picar4[i]))
-        k += 1
-        x_min += h
-
-        if k % 10 == 0:
-            tabl.add_row(row)
-
-
-    return tabl
-
-def task_3() -> PrettyTable:
-    tabl = PrettyTable()
-
-    tabl.field_names = ["Аргумент", "Эйлер", "Пикар 1", "Пикар 2", "Пикар 3", "Пикар 4"]
-
-    x_min = 0
-    x_max = 1.3
-    y_start = 0
-    h = 1e-3
-    n = int((x_max - x_min) / h)
-    eul = algos.euler(x_min, y_start, h, n, f_3)
-    picar1 = algos.picar(x_max, h, algos.picar_apox_31, 0, 0)
-    picar2 = algos.picar(x_max, h, algos.picar_apox_32, 0, 0)
-    picar3 = algos.picar(x_max, h, algos.picar_apox_33, 0, 0)
-    picar4 = algos.picar(x_max, h, algos.picar_apox_34, 0, 0)
-    k = -1
-    for i in range(n):
-        row = []
-        row.append("{:^9.3f}".format(x_min))
         if type(eul[i]) == int:
             if eul[i] > 10000000:
                 row.append("Too big")
@@ -120,6 +86,61 @@ def task_3() -> PrettyTable:
 
     return tabl
 
+def task_3() -> PrettyTable:
+    tabl = PrettyTable()
+
+    #tabl.field_names = ["Аргумент", "Эйлер"] #, "Пикар 1", "Пикар 2", "Пикар 3", "Пикар 4"]
+
+    x_min = 0
+    x_max = 2.004
+    y_start = 0
+    h = 1e-6
+    n = int((x_max - x_min) / h)
+    #eul = algos.euler(x_min, y_start, h, n, f_3)
+    ans = []
+    y = y_start
+    x = x_min
+    y0 = y_start
+    for i in range(n):
+        try:
+            y0 = y
+            y += h * f_3(x, y)
+            ans.append(y)
+            x += h
+        except OverflowError:
+            y = y0
+            print("{:^9.5e}".format(h), x, y)
+            h /= 2
+            #ans.append("Over")
+    # picar1 = algos.picar(x_max, h, algos.picar_apox_31, 0, 0)
+    # picar2 = algos.picar(x_max, h, algos.picar_apox_32, 0, 0)
+    # picar3 = algos.picar(x_max, h, algos.picar_apox_33, 0, 0)
+    # picar4 = algos.picar(x_max, h, algos.picar_apox_34, 0, 0)
+    k = -1
+    # for i in range(n):
+    #     row = []
+    #     row.append("{:^9.5f}".format(x_min))
+    #     if type(eul[i]) == int:
+    #         if eul[i] > 10000000:
+    #             row.append("Too big")
+    #         else:
+    #             row.append("{:^9.5f}".format(eul[i]))
+    #     else:
+    #         row.append(eul[i])
+
+    #     # row.append("{:^9.5f}".format(picar1[i]))
+    #     # row.append("{:^9.5f}".format(picar2[i]))
+    #     # row.append("{:^9.5f}".format(picar3[i]))
+    #     # row.append("{:^9.5f}".format(picar4[i]))
+    #     k += 1
+    #     x_min += h
+
+    #     if k % 1000 == 0:
+    #         tabl.add_row(row)
+
+
+    return tabl
+
 
 def main():
     task_num = int(input("Введите номер задания: "))
@@ -134,8 +155,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# for i in range(10):
-#     print(i / 10, analit.analit(i / 10))
